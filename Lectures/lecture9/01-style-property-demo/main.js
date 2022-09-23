541b920b6cd2747f82aab5a346755219e2f5d524
Davin <dshim@unca.edu>
2022-09-23 16:21:27 -0400

    tutorial6

---
+++ Lectures/lecture9/01-style-property-demo/main.js
@@ -0,0 +1,33 @@
+
+const makeRed = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'red';
+};
+
+const makeBlue = () => {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector('body').style.backgroundColor = 'blue';
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('body').style.backgroundColor = '#FFC0CB';
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('body').style.backgroundColor = 'orange';
+};
+
+const makeRainbow = () => {
+    // your code here...
+    console.log('Change boxes to rainbow');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+    document.querySelector('#section2').style.backgroundColor = 'orange';
+    document.querySelector('#section3').style.backgroundColor = 'yellow';
+    document.querySelector('#section4').style.backgroundColor = 'green';
+};
